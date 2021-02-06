8ac6cb4e91329524fde9b63c633e6a86e8364cb4
Adrien Luitot <23219818+adrienluitot@users.noreply.github.com>
2021-02-06 15:58:29 +0100

    update script for static-i18n v2.10

--- update_translation.sh
+++ update_translation.sh
@@ -28,10 +28,10 @@ done
 
 if [ "$all" = true ] || [ "$updateMain" = true ]; then
     echo "[Debug] Translate main page"
-    static-i18n -l en -i en -i fr --allowHtml --exclude projects --outputDir site html_templates
+    static-i18n -l en -i en -i fr --localesPath locales --allowHtml --exclude projects --outputDir site html_templates
 fi
 
 if [ "$all" = true ] || [ "$updateProjects" = true ]; then
     echo "[Debug] Translate projects"
-    static-i18n -l en -i en -i fr --allowHtml --exclude index.html --fixPaths false --outputDir site html_templates
+    static-i18n -l en -i en -i fr --localesPath locales --allowHtml --exclude index.html --fixPaths false --outputDir site html_templates
 fi
